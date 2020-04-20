e0cb4155fa3b360ff3a3112500d3ab4f87c70187
olivia-miao <63760068+olivia-miao@users.noreply.github.com>
2020-04-20 16:16:50 +0800

    add describing information of datasets

--- test.py
+++ test.py
@@ -1,8 +1,33 @@
 print("Hello")
-print("hard job")
 
 ### 任务1： 拷贝阿尔兹海默病人的源文件到这个文件下
 
 ### 任务2： 使用numpy或者pandas读取病人源文件
 
-print("miao is pretty!")
+
+# step1：读入table1_description.xlsx,这是一个描述性的table，表示我找到的数据集及他的相关信息
+import pandas as pd
+from pandas import DataFrame
+
+table1 = DataFrame(pd.read_excel('table1_description.xlsx'))
+print(table1)
+
+# step2：读入table2_demography.xlsx,这是对每一个数据集进行的描述
+table2 = DataFrame(pd.read_excel('table2_demography.xlsx'))
+print(table2)
+
+# step3： 读入table3_21brainRegions.xlsx，这是对每一个脑区的分组，接下来就是根据这个分组对每一个脑区分别进行分析
+# table3中有21个sheet，先得到每个sheet的名称，以及得到每个sheet中的内容
+
+# 方法一：
+x1 = pd.ExcelFile('table3_21brainRegions.xlsx')
+sheet_names = x1.sheet_names
+print(sheet_names)
+df = x1.parse(sheet_names)
+print(df)
+# 结果中为啥最后两个sheet的格式和前面不一样？
+
+# 方法二：
+# df = pd.read_excel('table3_21brainRegions.xlsx', None)
+# df.keys()
+# print(df)
